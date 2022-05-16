6d887ae4e93e522d0bbc8b1cbaf7d7e6358e4dcd
Ricky Chon <rickychon99@gmail.com>
2022-05-16 15:31:12 -0400

    Add test for pixelDiff util function

--- tests/util/pixel.test.js
+++ tests/util/pixel.test.js
@@ -15,3 +15,13 @@ describe('pixelsMatch', () => {
     expect(pixelsMatch(p1, p2)).toBeFalsy();
   });
 });
+
+describe('pixelDiff', () => {
+  it('should return the difference between pixels', () => {
+    const p1 = { r: 10, g: 10, b: 10, a: 0 };
+    const p2 = { r: 10, g: 5, b: 4, a: 0 };
+    const expected = { r: 0, g: 5, b: 6, a: 0 };
+
+    expect(pixelDiff(p1, p2)).toEqual(expected);
+  });
+});
